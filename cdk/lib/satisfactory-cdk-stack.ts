8e782fe353134b7570a9f62aeed5d188b91af560
charltona <2724511+charltona@users.noreply.github.com>
2021-11-21 21:07:49 +1000

    wip

--- cdk/lib/satisfactory-cdk-stack.ts
+++ cdk/lib/satisfactory-cdk-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
-import {RemovalPolicy} from '@aws-cdk/core';
+import * as path from 'path';
+import {Arn, ArnFormat, Duration, RemovalPolicy} from '@aws-cdk/core';
 import * as ec2 from "@aws-cdk/aws-ec2";
 import * as ecs from "@aws-cdk/aws-ecs";
 import {MountPoint, Protocol} from "@aws-cdk/aws-ecs";
@@ -150,12 +151,89 @@ export class SatisfactoryCdkStack extends cdk.Stack {
       securityGroups: [serviceSecurityGroup]
     })
 
+    const autoScaling = satisfactoryServerService.autoScaleTaskCount({
+      minCapacity: 0,
+      maxCapacity: 1,
+    });
+
+    autoScaling.scaleOnMetric('ScaleDownOnCpuUsage', {
+      metric: satisfactoryServerService.metric('CPUUtilization'),
+      scalingSteps: [
+        {upper: 10, change: -1},
+        {lower: 50, change: 0},
+      ],
+      cooldown: cdk.Duration.minutes(20),
+      evaluationPeriods: 4,
+    })
+
     fileSystem.connections.allowDefaultPortFrom(satisfactoryServerService.connections);
 
+    const hostedZoneId = r53.HostedZone.fromLookup(this, 'HostedZoneLookup', {
+      domainName: 'hamlet.link'
+    })
 
+    const watchDoggoContainer = new ecs.ContainerDefinition(this, 'WatchDoggoContainer', {
+      containerName: 'WatchDoggoContainer',
+      image: ecs.ContainerImage.fromAsset(
+          path.resolve(__dirname, '../../ficsit-watchdog')
+      ),
+      essential: false,
+      taskDefinition,
+      environment: {
+        CLUSTER: 'SatisfactoryCDKCluster',
+        SERVICE: 'SatisfactoryServerService',
+        DNSZONE: hostedZoneId.hostedZoneId,
+        SERVERNAME: `hamlet.link`,
+      },
+      logging: new ecs.AwsLogDriver({
+        logRetention: logs.RetentionDays.THREE_DAYS,
+        streamPrefix: 'WatchDoggoContainer',
+      })
+    })
 
-    // Autoscaling policy, if active connections === 0 for 30 mins, then set desiredCount to 0.
+    const serviceControlPolicy = new iam.Policy(this, 'ServiceControlPolicy', {
+      statements: [
+        new iam.PolicyStatement({
+          sid: 'AllowAllOnServiceAndTask',
+          effect: iam.Effect.ALLOW,
+          actions: ['ecs:*'],
+          resources: [
+            satisfactoryServerService.serviceArn,
+            Arn.format(
+                {
+                  service: 'ecs',
+                  resource: 'task',
+                  resourceName: `SatisfactoryCDKCluster/*`,
+                  arnFormat: ArnFormat.SLASH_RESOURCE_NAME,
+                },
+                this
+            ),
+          ],
+        }),
+        new iam.PolicyStatement({
+          effect: iam.Effect.ALLOW,
+          actions: ['ec2:DescribeNetworkInterfaces'],
+          resources: ['*'],
+        }),
+      ],
+    });
 
-    // Write lambda function to set desiredCount to 1.
+    serviceControlPolicy.attachToRole(ecsTaskRole);
+
+    const iamRoute53Policy = new iam.Policy(this, 'IamRoute53Policy', {
+      statements: [
+        new iam.PolicyStatement({
+          sid: 'AllowEditRecordSets',
+          effect: iam.Effect.ALLOW,
+          actions: [
+            'route53:GetHostedZone',
+            'route53:ChangeResourceRecordSets',
+            'route53:ListResourceRecordSets',
+          ],
+          resources: [`arn:aws:route53:::hostedzone/Z07326652201O046A8CXC`],
+        }),
+      ],
+    });
+    iamRoute53Policy.attachToRole(ecsTaskRole);
   }
 }
