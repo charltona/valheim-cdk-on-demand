06f212cd5679011ca3e3266ce2d9fe9c846f7acc
charltona <2724511+charltona@users.noreply.github.com>
2021-11-28 20:57:45 +1000

    update lambda launcher for discord bot

--- cdk/lib/launcher-stack.ts
+++ cdk/lib/launcher-stack.ts
@@ -1,8 +1,10 @@
 import * as cdk from '@aws-cdk/core';
-import * as lambda from "@aws-cdk/aws-lambda"
+import * as lambda from "@aws-cdk/aws-lambda-python"
+import {Runtime} from "@aws-cdk/aws-lambda";
 import * as logs from "@aws-cdk/aws-logs"
 import * as path from 'path';
-
+import * as apigw from '@aws-cdk/aws-apigatewayv2';
+import * as lambdaInt from "@aws-cdk/aws-apigatewayv2-integrations";
 
 
 export class LauncherStack extends cdk.Stack {
@@ -11,10 +13,11 @@ export class LauncherStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const launcherLambda = new lambda.Function(this, 'LauncherLambda', {
-      code: lambda.Code.fromAsset(path.resolve(__dirname, '../../lambda')),
-      handler: 'startup.lambda_handler',
-      runtime: lambda.Runtime.PYTHON_3_8,
+    const launcherLambda = new lambda.PythonFunction(this, 'LauncherLambda', {
+      entry: path.resolve(__dirname, '../../lambda'),
+      index: 'startup.py',
+      runtime: Runtime.PYTHON_3_8,
+      handler: 'lambda_handler',
       environment: {
         // @TODO Make configurable.
       },
@@ -22,6 +25,29 @@ export class LauncherStack extends cdk.Stack {
     })
 
     this.launcherLambdaRoleArn = launcherLambda.role!.roleArn;
+
+    const lambdaIntegration = new lambdaInt.LambdaProxyIntegration({
+      handler: launcherLambda
+    })
+
+    const httpApi = new apigw.HttpApi(this, 'LauncherHttpApi', {
+      corsPreflight: {
+        allowOrigins: ['*'],
+        allowMethods: [apigw.CorsHttpMethod.POST]
+      },
+      apiName: 'LauncherApi',
+      createDefaultStage: true,
+    });
+
+    httpApi.addRoutes({
+      path: '/',
+      methods: [
+          apigw.HttpMethod.POST,
+      ],
+      integration: lambdaIntegration
+    })
+
+    new cdk.CfnOutput(this, 'LauncherAPI', {value: httpApi.url!, exportName: 'LauncherAPIEndpoint'})
   }
 
 
--- lambda/startup.py
+++ lambda/startup.py
@@ -1,5 +1,22 @@
 import os
 import boto3
+import json
+import requests
+
+from nacl.signing import VerifyKey
+from nacl.exceptions import BadSignatureError
+from dotenv import load_dotenv
+load_dotenv()
+
+PUBLIC_KEY = os.environ.get('DISCORD_API_KEY') # found on Discord Application -> General Information page
+PING_PONG = {'statusCode': 200, 'body': json.dumps({'type': 1})}
+RESPONSE_TYPES =  {
+                    "PONG": 1,
+                    "ACK_NO_SOURCE": 2,
+                    "MESSAGE_NO_SOURCE": 3,
+                    "MESSAGE_WITH_SOURCE": 4,
+                    "ACK_WITH_SOURCE": 5
+                  }
 
 DEFAULT_REGION = 'ap-southeast-2'
 DEFAULT_CLUSTER = 'SatisfactoryCDKCluster'
@@ -9,11 +26,33 @@ REGION = os.environ.get('REGION', DEFAULT_REGION)
 CLUSTER = os.environ.get('CLUSTER', DEFAULT_CLUSTER)
 SERVICE = os.environ.get('SERVICE', DEFAULT_SERVICE)
 
-if REGION is None or CLUSTER is None or SERVICE is None:
+if REGION is None or CLUSTER is None or SERVICE is None or PUBLIC_KEY is None:
     raise ValueError("Missing environment variables")
+
+def ping_pong(body):
+    jsonbody = json.loads(body)
+    if jsonbody["type"] == 1:
+        return True
+
+def verify_signature(event):
+    body = event['body']
+    signature = event['headers']['x-signature-ed25519']
+    timestamp  = event['headers']['x-signature-timestamp']
+
+    verify_key = VerifyKey(bytes.fromhex(PUBLIC_KEY))
+    verify_key.verify(f'{timestamp}{body}'.encode(), bytes.fromhex(signature)) # raises an error if unequal
 
 
 def lambda_handler(event, context):
+    try:
+        verify_signature(event)
+    except Exception as e:
+        raise Exception(f"[UNAUTHORISED] Invalid Request Signature: {e}")
+
+    body = event['body']
+    if ping_pong(body):
+        return PING_PONG
+
     """Updates the desired count for a service."""
 
     ecs = boto3.client('ecs', region_name=REGION)
@@ -22,14 +61,60 @@ def lambda_handler(event, context):
         services=[SERVICE],
     )
 
+    jsonbody = json.loads(body)
+
+    command = jsonbody['data']['options'][0]['value']
+    interaction_id = jsonbody['id']
+    interaction_token = jsonbody['token']
+
     desired = response["services"][0]["desiredCount"]
 
-    if desired == 0:
-        ecs.update_service(
-            cluster=CLUSTER,
-            service=SERVICE,
-            desiredCount=1,
-        )
-        print("Updated desiredCount to 1")
+    if command == 'start':
+        if desired == 0:
+            ecs.update_service(
+                cluster=CLUSTER,
+                service=SERVICE,
+                desiredCount=1,
+            )
+            message = "Starting Satisfactory Server"
+            print("Updated desiredCount to 1")
+        else:
+            message = "Satisfactory Server already running - try again later."
+            print("desiredCount already at 1")
+    elif command == 'stop':
+        if desired == 1:
+            ecs.update_service(
+                cluster=CLUSTER,
+                service=SERVICE,
+                desiredCount=0,
+            )
+            message = "Stopping Satisfactory Server"
+            print("Updated desiredCount to 0")
+        else:
+            message = "Satisfactory Server already stopped - try again later."
+            print("desiredCount already at 0")
     else:
-        print("desiredCount already at 1")
+            message = "Beep boop. Invalid command - try 'start' or 'stop'."
+
+    url = f"https://discord.com/api/v8/interactions/{interaction_id}/{interaction_token}/callback"
+    payload = {
+        "type": 4,
+        "data": {
+            "content": message
+        }
+    }
+
+    r = requests.post(url, json=payload)
+
+    return {
+        "statusCode": 200,
+        'body': json.dumps({
+            "type": RESPONSE_TYPES['MESSAGE_WITH_SOURCE'],
+            "data": {
+                "tts": False,
+                "content": message,
+                "embeds": [],
+                "allowed_mentions": []
+            }
+        })
+    }
